8e49250d94d2ccbd8ed9b7e183e5035273765eac
JvFern4ndes <jvfernandes.contato@gmail.com>
2024-06-26 19:55:34 -0300

    feat: create a getTweet function

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -10,4 +10,8 @@ contract Twitter {
         tweets[msg.sender] = _tweet;
     }
 
+    function getTweet(address _owner) public view returns (string memory) {
+        return tweets[_owner];
+    }
+
 }
